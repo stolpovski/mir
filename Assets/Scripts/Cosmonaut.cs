6778e87548f38b4f065173d410c1f50168c79b80
stolpovski <stolpovski@gmail.com>
2023-05-06 23:40:52 +0300

    controls

--- Assets/Scripts/Cosmonaut.cs
+++ Assets/Scripts/Cosmonaut.cs
@@ -1,33 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Cosmonaut : MonoBehaviour
 {
     Rigidbody rb;
-    float move;
+    
+    float thrust;
+    float yaw;
+    float lift;
 
-    public float speed = 1f;
-    // Start is called before the first frame update
+    public float force = 1f;
+    public float torque = 0.001f;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddRelativeForce(Vector3.forward * move * speed, ForceMode.Impulse);
+        rb.AddRelativeForce(Vector3.forward * thrust * force, ForceMode.Impulse);
+        rb.AddRelativeForce(Vector3.up * lift * force, ForceMode.Impulse);
+        rb.AddRelativeTorque(Vector3.up * yaw * torque, ForceMode.Impulse);
     }
 
     private void Update()
     {
-        Debug.Log(rb.velocity.magnitude);
+        //Debug.Log(rb.velocity.magnitude);
+        Debug.Log(rb.angularVelocity.magnitude);
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Thrust(InputAction.CallbackContext context)
     {
-        move = context.ReadValue<float>();
+        thrust = context.ReadValue<float>();
+    }
+
+    public void Lift(InputAction.CallbackContext context)
+    {
+        lift = context.ReadValue<float>();
+    }
+
+    public void Yaw(InputAction.CallbackContext context)
+    {
+        yaw = context.ReadValue<float>();
     }
 }
